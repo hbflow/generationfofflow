c475fbe5b80a0b4c8dbc13a96700b5ead13041c1
Your <you@example.com>
2021-06-08 04:28:21 -0400

    Added and implemented Delayed_Widget Package

--- lib/widgets/fullscreencentertext.dart
+++ lib/widgets/fullscreencentertext.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:delayed_widget/delayed_widget.dart';
 
 class fstext extends StatelessWidget {
   final text;
@@ -7,13 +8,19 @@ class fstext extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: SelectableText(
-          '${text}',
-          style: TextStyle(
-              fontWeight: FontWeight.w900,
-              fontSize: MediaQuery.of(context).size.width / 20),
+    //The DelayedWidget will be used currently but I will soon create the slide from bottom animation myself
+    return DelayedWidget(
+      delayDuration: Duration(milliseconds: 200), 
+      animationDuration: Duration(seconds: 1), 
+      animation: DelayedAnimations.SLIDE_FROM_BOTTOM,
+      child: Center(
+        child: Container(
+          child: SelectableText(
+            '${text}',
+            style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: MediaQuery.of(context).size.width / 20),
+          ),
         ),
       ),
     );
