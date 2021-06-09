384a6bfa4029808c91431890b309153bfbefdb87
Your <you@example.com>
2021-06-09 11:55:12 -0400

    tap anywhere

--- lib/pages/firstpage.dart
+++ lib/pages/firstpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hybriidflow/widgets/fullscreencentertext.dart';
+import 'dart:async';
 
 class firstpage extends StatefulWidget {
   @override
@@ -7,13 +8,35 @@ class firstpage extends StatefulWidget {
 }
 
 class _firstpageState extends State<firstpage> {
+  String hybriidtext = "Let's get you Started :)";
+
+  Future<void> playy() async {
+    print('2nd working');
+
+    Future.delayed(const Duration(seconds: 5), () {
+      setState(() {
+        hybriidtext = "Tap Anywhere";
+      });
+    });
+  }
+
+  void initState() {
+    super.initState();
+    playy();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: fstext(
-        text: ('Welcome to HybriidFlow'),
-        text2: ("Let's get you Started :)"),
+    return GestureDetector(
+      onTap: () {
+        print('Any where app');
+      },
+      child: Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: fstext(
+          text: ('Welcome to HybriidFlow'),
+          text2: (hybriidtext),
+        ),
       ),
     );
   }
